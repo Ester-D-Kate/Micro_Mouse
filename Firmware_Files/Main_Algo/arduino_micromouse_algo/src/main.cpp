2169c1cd4f9339b3de25c5df91523b2f9e640e8b
Ester-D-Kate <148463665+Ester-D-Kate@users.noreply.github.com>
2025-03-14 16:41:09 +0530

    arduino code for main algo is completed

--- Firmware_Files/Main_Algo/arduino_micromouse_algo/src/main.cpp
+++ Firmware_Files/Main_Algo/arduino_micromouse_algo/src/main.cpp
@@ -1,4 +1,18 @@
 #include <Arduino.h>
+#include <NewPing.h>
+
+#define trigPinLeft 14
+#define echoPinLeft 15
+#define trigPinFront 16
+#define echoPinFront 17
+#define trigPinRight 18
+#define echoPinRight 19
+
+#define IR_LEFT A6 
+#define IR_RIGHT A7
+
+#define RIGHT_HALL 12  
+#define LEFT_HALL 11  
 
 #define LEFT_MOTOR_FWD 8
 #define LEFT_MOTOR_BWD 9
@@ -7,11 +21,26 @@
 #define PWM_LEFT 10
 #define PWM_RIGHT 5
 
+#define MAX_DISTANCE 200 // Max distance in cm
+
+int rightCount = 0;  
+int leftCount = 0; 
+bool rightLastState = HIGH; 
+bool leftLastState = HIGH;
+
+int irLeftValue = 0;
+int irRightValue = 0;
+
+String command = "<0,0,0,0,0,0,0>";
 String inputData = "";
 int rightSpeedValue = 0; 
 int leftSpeedValue = 0; 
 char direction = 'S';
 char turnDir = 'N';
+
+NewPing SonarFront(trigPinFront, echoPinFront, MAX_DISTANCE);
+NewPing SonarRight(trigPinRight, echoPinRight, MAX_DISTANCE);
+NewPing SonarLeft(trigPinLeft, echoPinLeft, MAX_DISTANCE);
 
 void controlMotors() {
   int leftSpeed = leftSpeedValue;
@@ -56,8 +85,11 @@ switch (direction) {
 }
 
 void setup() {
-    Serial.begin(115200);
-
+    Serial.begin(57600);
+    
+    
+    pinMode(RIGHT_HALL, INPUT_PULLUP); 
+    pinMode(LEFT_HALL, INPUT_PULLUP);
     pinMode(LEFT_MOTOR_FWD, OUTPUT);
     pinMode(LEFT_MOTOR_BWD, OUTPUT);
     pinMode(RIGHT_MOTOR_FWD, OUTPUT);
@@ -67,19 +99,54 @@ void setup() {
 }
 
 void loop() {
+    
+    int DisFront = SonarFront.ping_cm();
+    int DisRightDiagnal = SonarRight.ping_cm();
+    int DisLeftDiagnal = SonarLeft.ping_cm();
+    if (DisFront == 0) DisFront = MAX_DISTANCE;
+    if (DisRightDiagnal == 0) DisRightDiagnal = MAX_DISTANCE;
+    if (DisLeftDiagnal == 0) DisLeftDiagnal = MAX_DISTANCE;
+    
+    bool rightCurrentState = digitalRead(RIGHT_HALL);
+    bool leftCurrentState = digitalRead(LEFT_HALL);
+    if (rightLastState == LOW && rightCurrentState == HIGH) {
+        rightCount++; 
+    }  
+    if (leftLastState == LOW && leftCurrentState == HIGH) {
+        leftCount++; // Increment left count
+    }
+    rightLastState = rightCurrentState;
+    leftLastState = leftCurrentState;
+
+    irLeftValue = analogRead(IR_LEFT); 
+    irRightValue = analogRead(IR_RIGHT);
+    if(irLeftValue > 720) {
+        irLeftValue = 1;
+    }else if(irLeftValue < 700) {
+        irLeftValue = 0;
+    }
+    if(irRightValue > 720) {
+        irRightValue = 1;
+    }else if(irRightValue < 700) {
+        irRightValue = 0;
+    }
+
+
+    command = "<" + String(DisFront) + "," + String(DisLeftDiagnal) + "," + String(DisRightDiagnal) + "," + 
+          String(leftCount) + "," + String(rightCount) + "," + 
+          String(irLeftValue) + "," + String(irRightValue) + ">";
+    Serial.println(command);
+    
+    
+    char inputBuffer[30]; // enough length for command
+
     if (Serial.available()) {
-        inputData = Serial.readStringUntil('\n'); // Read command from ESP8266
+        int len = Serial.readBytesUntil('\n', inputBuffer, sizeof(inputBuffer));
+        inputBuffer[len] = '\0'; // null-terminate the string
 
-        if (inputData.length() > 5) {
-            // Parse values from the command format: "<dir,rightSpeed,leftSpeed,N,0>"
-            direction = inputData.charAt(1);
-            int firstComma = inputData.indexOf(',');
-            int secondComma = inputData.indexOf(',', firstComma + 1);
-            int thirdComma = inputData.indexOf(',', secondComma + 1);
-
-            leftSpeedValue = inputData.substring(firstComma + 1, secondComma).toInt();
-            rightSpeedValue = inputData.substring(secondComma + 1, thirdComma).toInt();
-            turnDir = inputData.charAt(thirdComma + 1);
+        if (len > 5) {
+            direction = inputBuffer[1];
+            sscanf(inputBuffer, "<%*c,%d,%d,%c", &leftSpeedValue, &rightSpeedValue, &turnDir);
         }
     }
 
